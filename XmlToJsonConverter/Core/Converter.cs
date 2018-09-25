322b1fd9ea5b5752aea7a0da247b14f52e2030f3
Avikik <pro100webber@gmail.com>
2018-09-25 19:30:57 +0700

    Update Converter.cs

--- XmlToJsonConverter/Core/Converter.cs
+++ XmlToJsonConverter/Core/Converter.cs
@@ -8,26 +8,35 @@ namespace XmlToJsonConverter.Core
     {
         private string inputFileName;
         private string outPutFileName;
+        private string[] args;
         FilterArgsData filter;
+        
         public TextConverter(string[] args)
         {
-            filter = new FilterArgsData(args);
-            inputFileName = args[0];
-            outPutFileName = args[1];                    
+            this.args = args;
+            filter = new FilterArgsData(args);                             
         }
 
         public void StartConverter()
         {
+            if (isInputDataTrue())
+                ConvertedXmlToJson(inputFileName, outPutFileName);
+        }
+
+        private bool isInputDataTrue()
+        {
             if (filter.isFilterArgs())
             {
-                ConvertedXmlToJson(inputFileName, outPutFileName);
+                inputFileName = args[0];
+                outPutFileName = args[1];
+                return true;
             }
+            return false;
         }
 
         private void ConvertedXmlToJson(string inputFileName, string outPutFileName)
         {
             JsonCreater.CreateJsonFile(XmlParser.ParseXmlDocument(inputFileName), outPutFileName);
-        }
-       
+        }       
     }
 }
